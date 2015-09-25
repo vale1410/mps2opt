bd0c46c8c51c00f7a5f8d8083599f367bcd7f268
vale <vale1410@gmail.com>
2015-09-25 18:22:04 +1000

    Added converted

--- README.md
+++ README.md
@@ -1,2 +1,98 @@
 # mps2opt
+
 An simple .mps MIPLIB converter to other formats. 
+
+Currently supports conversion to minizinc format of MIPLIB mps files of type BP
+and IP with integer coefficients. This converter is pre-alpha and works only under certain assumptions. 
+
+To install download the golang distribution and use *go get*. 
+
+```bash
+go get git@github.com:vale1410/mps2opt.git
+```
+
+```bash
+mps2opt -f air04.mps -minizinc
+```
+
+Please report any errors you encounter. 
+
+Converted Instances
+-------------------
+
+It has converted successfully the following MIPLIB instances into minizinc: 
+
+* air04.mps
+* ash608gpia-3col.mps
+* eil33-2.mps
+* eilA101-2.mps
+* eilB101.mps
+* ex1010-pi.mps
+* ex10.mps
+* ex9.mps
+* f2000.mps
+* go19.mps
+* hanoi5.mps
+* iis-100-0-cov.mps
+* iis-bupa-cov.mps
+* iis-pima-cov.mps
+* m100n500k4r1.mps
+* macrophage.mps
+* methanosarcina.mps
+* mine-166-5.mps
+* mine-90-10.mps
+* neos-1109824.mps
+* neos-1337307.mps
+* neos-1440225.mps
+* neos-1616732.mps
+* neos-1620770.mps
+* neos18.mps
+* neos-631710.mps
+* neos-777800.mps
+* neos-785912.mps
+* neos-807456.mps
+* neos-820146.mps
+* neos-820157.mps
+* neos-849702.mps
+* neos858960.mps
+* neos-952987.mps
+* neos-957389.mps
+* opm2-z10-s2.mps
+* opm2-z11-s8.mps
+* opm2-z12-s14.mps
+* opm2-z12-s7.mps
+* opm2-z7-s2.mps
+* p6b.mps
+* queens-30.mps
+* rail01.mps
+* ramos3.mps
+* reblock166.mps
+* reblock354.mps
+* reblock420.mps
+* reblock67.mps
+* seymour.mps
+* stp3d.mps
+* sts405.mps
+* sts729.mps
+* t1717.mps
+* t1722.mps
+* tanglegram1.mps
+* tanglegram2.mps
+* toll-like.mps
+* vpphard.mps
+* 30n20b8.mps
+* d10200.mps
+* d20200.mps
+* lectsched-1.mps
+* lectsched-1-obj.mps
+* lectsched-2.mps
+* lectsched-3.mps
+* lectsched-4-obj.mps
+* neos16.mps
+* neos-686190.mps
+* ns1854840.mps
+* rococoB10-011000.mps
+* rococoC10-001000.mps
+* rococoC11-011100.mps
+* rococoC12-111000.mps
+
---
+++ mps2opt.go
@@ -0,0 +1,470 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"flag"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var ver = flag.Bool("ver", false, "Show version info.")
+var filename = flag.String("f", "", "Path of the file.")
+var gringo = flag.Bool("gringo", false, "Ouput in Potasscos Gringo Format.")
+var minizinc = flag.Bool("minizinc", true, "Ouput in Minizinc Format.")
+var maxDom = int64(1000)
+
+func main() {
+
+	flag.Parse()
+
+	var pbs []Linear
+	var vars map[string]Bound
+	var err error
+
+	if *ver {
+		fmt.Println(`MIPLIB .mps converter: Tag 0.1
+Copyright (C) Data61 and Valentin Mayer-Eichberger
+License GPLv2+: GNU GPL version 2 or later <http://gnu.org/licenses/gpl.html>
+There is NO WARRANTY, to the extent permitted by law.`)
+		return
+	}
+
+	f := ""
+	if strings.HasSuffix(flag.Arg(0), "mps") {
+		f = flag.Arg(0)
+	} else if strings.HasSuffix(*filename, "mps") {
+		f = *filename
+	}
+
+	if f != "" {
+		pbs, vars, err = ParseMPS(f)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(100)
+			return
+		}
+
+		if *minizinc {
+			PrintMinizinc(pbs, vars)
+		} else if *gringo {
+			PrintGringo(pbs, vars)
+		}
+	} else {
+		os.Exit(100)
+	}
+
+	return
+}
+
+type Constraint struct {
+	name string
+}
+
+type EquationType int
+
+const (
+	LE  EquationType = iota //"<="
+	GE                      //">="
+	EQ                      //"=="
+	OPT                     //"MIN"
+)
+
+func (e EquationType) String() string {
+	switch e {
+	case LE:
+		return "<="
+	case GE:
+		return ">="
+	case EQ:
+		return "=="
+	case OPT:
+		return "MIN"
+	}
+	return ""
+}
+
+type Bound struct {
+	lb, ub int64
+}
+
+type Entry struct {
+	id     string
+	Weight int64
+}
+
+type Linear struct {
+	Desc    string
+	Entries []Entry
+	K       int64
+	Typ     EquationType
+}
+
+func getEquationType(s string) EquationType {
+	if s == "E" {
+		return EQ
+	} else if s == "L" {
+		return LE
+	} else if s == "G" {
+		return GE
+	}
+	if s != "N" {
+		panic("Unknown equation type " + s)
+	}
+	return OPT
+}
+
+func PrintMinizinc(pbs []Linear, vars map[string]Bound) {
+
+	for x, b := range vars {
+		fmt.Println("var", b.lb, "..", b.ub, ":", x, ";")
+	}
+
+	for _, t := range pbs {
+		if len(t.Entries) > 0 {
+
+			switch t.Typ {
+			case LE:
+				fmt.Print("constraint ")
+			case GE:
+				fmt.Print("constraint ")
+			case EQ:
+				fmt.Print("constraint ")
+			case OPT:
+				fmt.Print("solve minimize ")
+			}
+
+			for i, e := range t.Entries {
+				if i == 0 {
+					if e.Weight > 0 {
+						if e.Weight == 1 {
+							fmt.Print(e.id)
+						} else {
+							fmt.Print(e.Weight, "*", e.id)
+						}
+					} else {
+						fmt.Print(e.Weight, "*", e.id)
+					}
+				} else {
+					if e.Weight > 0 {
+						if e.Weight == 1 {
+							fmt.Print(" + ", e.id)
+						} else {
+							fmt.Print(" + ", e.Weight, "*", e.id)
+						}
+					} else {
+						if e.Weight == -1 {
+							fmt.Print(" - ", e.id)
+						} else {
+							fmt.Print(" - ", -e.Weight, "*", e.id)
+						}
+					}
+				}
+			}
+
+			switch t.Typ {
+			case LE:
+				fmt.Print(" <= ", t.K, ";\n")
+			case GE:
+				fmt.Print(" >= ", t.K, ";\n")
+			case EQ:
+				fmt.Print(" == ", t.K, ";\n")
+			case OPT:
+				fmt.Print(" ;\n")
+			}
+		}
+	}
+
+	//fmt.Print("output [")
+	//first := true
+	//for x, _ := range vars {
+	//	if first {
+	//		first = false
+	//	} else {
+	//		fmt.Print(",")
+	//	}
+	//	fmt.Print("\"", x, "\",\"=\",", "show(", x, "),\"\\n\"\n")
+	//}
+	//fmt.Println("];")
+}
+
+func PrintGringo(pbs []Linear, vars map[string]Bound) {
+
+	fmt.Println("#hide.")
+
+	for x, b := range vars {
+
+		if b.ub-b.lb > maxDom {
+			fmt.Println("Variable domain exeeds maxDom: ", b.ub, b.lb)
+			panic("Variable domain too big")
+		}
+
+		if b.lb == 0 && b.ub == 1 {
+			fmt.Println("{", x, "}.")
+		} else {
+			fmt.Println(x+"_ge_"+strconv.FormatInt(b.lb, 10), ".")
+			for d := b.lb + 1; d <= b.ub; d++ {
+				fmt.Println("{", x+"_ge_"+strconv.FormatInt(d, 10), "}.")
+				if d != b.ub {
+					fmt.Println(":- not", x+"_ge_"+strconv.FormatInt(d, 10), ",", x+"_ge_"+strconv.FormatInt(d+1, 10), ".")
+				}
+			}
+		}
+	}
+	for _, t := range pbs {
+		if len(t.Entries) > 0 {
+
+			switch t.Typ {
+			case LE:
+				fmt.Print(":- ", t.K+1, " [ ")
+			case GE:
+				fmt.Print(":- [ ")
+			case EQ:
+				fmt.Print(":- not ", t.K, " [ ")
+			case OPT:
+				fmt.Print("#minimize[")
+			}
+
+			for i, e := range t.Entries {
+				if i != 0 {
+					fmt.Print(" , ")
+				}
+				b := vars[e.id]
+				if b.lb == 0 && b.ub == 1 {
+					fmt.Print(e.id, "=", e.Weight)
+				} else {
+					//if b.lb != 1 {
+					//	fmt.Print(" true=", b.lb-1, " , ")
+					//}
+					for d := b.lb + 1; d <= b.ub; d++ {
+						fmt.Print(e.id+"_ge_"+strconv.FormatInt(d, 10), "=", e.Weight)
+						if d != b.ub {
+							fmt.Print(" , ")
+						}
+					}
+				}
+			}
+
+			switch t.Typ {
+			case LE:
+				fmt.Print(" ]")
+			case GE:
+				fmt.Print(" ] ", t.K-1)
+			case EQ:
+				fmt.Print(" ] ", t.K)
+			case OPT:
+				fmt.Print("]")
+			}
+			fmt.Println(". %", t.Desc)
+		}
+	}
+}
+
+func reformatIdentifier(s string) string {
+	s = strings.ToLower(strings.Replace(s, "#", "_", -1))
+	s = strings.Replace(s, ",", "_", -1)
+	s = strings.Replace(s, "(", "_", -1)
+	s = strings.Replace(s, ")", "_", -1)
+	return s
+}
+
+func ParseMPS(f string) (pbs []Linear, vars map[string]Bound, err error) {
+	input, err2 := os.Open(f)
+	defer input.Close()
+	if err2 != nil {
+		err = errors.New("Please specifiy correct path to instance. Does not exist")
+		return
+	}
+	scanner := bufio.NewScanner(input)
+
+	pbs = make([]Linear, 0)
+	vars = make(map[string]Bound)
+	rowMap := make(map[string]int)
+
+	state := 0
+	i := -1
+
+	for scanner.Scan() {
+		i++
+		l := strings.Trim(scanner.Text(), " ")
+		if l == "" {
+			continue
+		}
+
+		entries := strings.Fields(l)
+		ts := Linear{}
+
+		switch state {
+		case 0:
+			{
+				if entries[0] == "NAME" {
+					state = 1
+				}
+				//fmt.Println("name : ", entries[1])
+			}
+		case 1:
+			{
+				if entries[0] == "ROWS" {
+					state = 2
+				}
+			}
+		case 2: // rows
+			if entries[0] == "COLUMNS" {
+				state = 3
+			} else {
+				ts.Typ = getEquationType(entries[0])
+				//ts.Desc = entries[1] strings.HasPrefix(entries[0], "LI")
+				row := reformatIdentifier(entries[1])
+				ts.Desc = row
+				pbs = append(pbs, ts)
+				rowMap[row] = len(pbs) - 1
+			}
+		case 3: // COLUMNS
+			if entries[0] == "INT1" || entries[0] == "INT1END" || entries[0] == "INTEND" ||
+				entries[0] == "INTSTART" || strings.HasPrefix(entries[0], "MARK") {
+				// do nothing
+				state = 3
+			} else if entries[0] == "RHS" {
+				state = 4
+			} else {
+
+				v := reformatIdentifier(entries[0])
+
+				if _, ok := vars[v]; !ok {
+					vars[v] = Bound{0, math.MaxInt64}
+				}
+
+				row := reformatIdentifier(entries[1])
+				//fmt.Println(v, row)
+				a, err2 := strconv.ParseInt(entries[2], 10, 64)
+				// also parse Float64 and convert?
+				if err2 != nil {
+					fmt.Println("Problem in line", i, ":", l)
+					err = errors.New("wrong number " + entries[2])
+					return
+				}
+
+				e := Entry{v, a}
+				pbs[rowMap[row]].Entries =
+					append(pbs[rowMap[row]].Entries, e)
+
+				if len(entries) == 5 {
+					a, err = strconv.ParseInt(entries[4], 10, 64)
+					// also parse Float64 and convert?
+					if err != nil {
+						fmt.Println("Problem in line", i, ":", l)
+						err = errors.New("wrong number " + entries[4])
+					}
+
+					e = Entry{v, a}
+					row = reformatIdentifier(entries[3])
+					//fmt.Println(v, row)
+					pbs[rowMap[row]].Entries =
+						append(pbs[rowMap[row]].Entries, e)
+				}
+			}
+		case 4:
+			{
+				if entries[0] == "BOUNDS" {
+					state = 5
+				} else if entries[0] == "RANGE" {
+					fmt.Println("Problem in line", i, ":", l)
+					err = errors.New("RANGE not supported yet in translator")
+					return
+				} else {
+					a, err2 := strconv.ParseInt(entries[2], 10, 64)
+					if err2 != nil {
+						fmt.Println("Problem in line", i, ":", l)
+						err = errors.New("wrong number " + entries[2])
+						return
+					}
+					row := reformatIdentifier(entries[1])
+					pbs[rowMap[row]].K = a
+					if len(entries) == 5 {
+						a, err2 = strconv.ParseInt(entries[4], 10, 64)
+						// also parse Float64 and convert?
+						if err2 != nil {
+							fmt.Println("Problem in line", i, ":", l)
+							err = errors.New("wrong number " + entries[4])
+							return
+						}
+
+						row = reformatIdentifier(entries[3])
+						//fmt.Println(v, row)
+						pbs[rowMap[row]].K = a
+					}
+				}
+			}
+		case 5:
+			{
+				if entries[0] == "ENDATA" {
+					state = 6
+					continue
+				}
+
+				//LO    lower bound        b <= x
+				//UP    upper bound        x <= b
+				//FX    fixed variable     x = b
+				//FR    free variable
+				//MI    lower bound -inf   -inf < x
+				//BV    binary variable    x = 0 or 1
+
+				if len(entries) <= 2 || len(entries) >= 5 {
+					fmt.Println("Problem in line", i, ":", l)
+					err = errors.New("wrong number of fields")
+					return
+				}
+
+				id := reformatIdentifier(entries[2])
+				b, ok := vars[id]
+				if !ok {
+					fmt.Println("Problem in line", i, ":", l)
+					err = errors.New("variable not existing " + id)
+					return
+				}
+
+				if strings.HasPrefix(entries[0], "BV") {
+					b = Bound{0, 1}
+				} else if strings.HasPrefix(entries[0], "FX") {
+					value, err2 := strconv.ParseInt(entries[3], 10, 64)
+					if err2 != nil {
+						fmt.Println("Problem in line", i, ":", l)
+						err = errors.New("wrong number " + entries[3])
+						return
+					}
+					b = Bound{value, value}
+				} else if strings.HasPrefix(entries[0], "LO") {
+					value, err2 := strconv.ParseInt(entries[3], 10, 64)
+					if err2 != nil {
+						fmt.Println("Problem in line", i, ":", l)
+						err = errors.New("wrong number " + entries[3])
+						return
+					}
+					b.lb = value
+				} else if strings.HasPrefix(entries[0], "UP") {
+					value, err2 := strconv.ParseInt(entries[3], 10, 64)
+					if err2 != nil {
+						fmt.Println("Problem in line", i, ":", l)
+						err = errors.New("wrong number " + entries[3])
+						return
+					}
+					b.ub = value
+				} else {
+					fmt.Println("Problem in line", i, ":", l)
+					err = errors.New("not supported yet in translator.")
+					return
+				}
+
+				vars[id] = b
+
+			}
+		default:
+			{
+			}
+		}
+	}
+	return
+}
